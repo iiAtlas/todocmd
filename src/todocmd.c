8d8889c8f87fdaabc7c58c9335356aa691070069
Walter McLallen <wallymclallen@icloud.com>
2013-06-17 18:17:16 -0400

    Added Folders and Main File

---
+++ src/todocmd.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[]) {
+        char* todofile = "todo.txt"
+        char* completedfile = "done.txt"
+
+        exit(EXIT_SUCCESS);
+}
